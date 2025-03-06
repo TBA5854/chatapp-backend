cc1253bac92968f915eda7602f00380500853e06
TBA5854 <adhavan5030@gmail.com>
2025-03-06 09:53:02 +0530

    fix: message controller uuid

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -25,7 +25,7 @@ export async function messageHandler(msg: Message, isOnline: boolean): Promise<M
                 sender,
                 reciever,
                 time,
-                message_id: `${sender}-${reciever}-${time}-${randomUUID}`,
+                message_id: `${sender}-${reciever}-${time}-${randomUUID()}`,
                 is_sent: isOnline,
                 replied_to
             },
